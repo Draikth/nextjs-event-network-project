f29043cde0f516346217cb6ec2a1c0fc6ed42f79
Marais Devis <maraisdcdevis@gmail.com>
2024-07-18 13:04:26 +0200

    Testing push after update with commented out warn.

--- app/post/PostEventForm.tsx
+++ app/post/PostEventForm.tsx
@@ -1,22 +1,22 @@
-'use client';
+// 'use client';
 
-import { useState } from 'react';
-import { Event } from '../../database/events';
+// import { useState } from 'react';
+// import { Event } from '../../database/events';
 
-type Props = {
-  events: Event[];
-};
+// type Props = {
+//   events: Event[];
+// };
 
-export default function RegisterForm(props: Props) {
-  const [name, setName] = useState('');
-  const [type, setType] = useState('');
-  const [date, setDate] = useState('');
-  const [location, setLocation] = useState('');
-  const [duration, setDuration] = useState('');
-  const [entryFee, setEntryFee] = useState('');
-  const [category, setCategory] = useState('');
-  const [description, setDescription] = useState('');
-  const [image, setImage] = useState('');
-  const [organizerUrl, setOrganizerUrl] = useState('');
-  const [ageRestriction, setAgeRestriction] = useState('');
-}
+// export default function RegisterForm(props: Props) {
+//   const [name, setName] = useState('');
+//   const [type, setType] = useState('');
+//   const [date, setDate] = useState('');
+//   const [location, setLocation] = useState('');
+//   const [duration, setDuration] = useState('');
+//   const [entryFee, setEntryFee] = useState('');
+//   const [category, setCategory] = useState('');
+//   const [description, setDescription] = useState('');
+//   const [image, setImage] = useState('');
+//   const [organizerUrl, setOrganizerUrl] = useState('');
+//   const [ageRestriction, setAgeRestriction] = useState('');
+// }
